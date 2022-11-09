73181f9dc7833048b650fc6b9f63bccc21c49a66
James Grieve <jgrieve@ualberta.ca>
2022-11-08 19:48:23 -0700

    Concatenation; String interpolation; Parameterized output;

--- EXSM3941/Program.cs
+++ EXSM3941/Program.cs
@@ -5,15 +5,20 @@
         static void Main(string[] args)
         // The start of Main() is the entry point of the program. Execution starts here when we run the program.
         {
-            Console.Write("Please enter a word: ");
-            // Trim the whitespace off and make it capitalized.
-            string myString = Console.ReadLine().Trim().ToUpper();
-
+            Console.Write("Please enter a first word: ");
+            string myString1 = Console.ReadLine().Trim().ToUpper();
+            Console.Write("Please enter a second word: ");
+            string myString2 = Console.ReadLine().Trim().ToUpper();
 
             Console.WriteLine("You entered this:");
-            Console.WriteLine(myString);
+            // Concatenation
+            Console.WriteLine(myString1+" "+myString2);
+            // Parameterized Output
+            Console.WriteLine("{0} {1}", myString1, myString2);
+            // String Interpolation
+            Console.WriteLine($"{myString1} {myString2}");
 
-        // The end of Main() is the exit point of the program. The program ends naturally when we get here.
+            // The end of Main() is the exit point of the program. The program ends naturally when we get here.
         }
     }
 }

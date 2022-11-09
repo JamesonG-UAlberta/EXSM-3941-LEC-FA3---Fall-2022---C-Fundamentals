c7e6c25366c6e1c40a38059003d643ce12814851
James Grieve <jgrieve@ualberta.ca>
2022-11-08 19:45:18 -0700

    Basic variables; User interaction; Trim and case changing;

--- EXSM3941/Program.cs
+++ EXSM3941/Program.cs
@@ -3,8 +3,17 @@
     internal class Program
     {
         static void Main(string[] args)
+        // The start of Main() is the entry point of the program. Execution starts here when we run the program.
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Please enter a word: ");
+            // Trim the whitespace off and make it capitalized.
+            string myString = Console.ReadLine().Trim().ToUpper();
+
+
+            Console.WriteLine("You entered this:");
+            Console.WriteLine(myString);
+
+        // The end of Main() is the exit point of the program. The program ends naturally when we get here.
         }
     }
 }

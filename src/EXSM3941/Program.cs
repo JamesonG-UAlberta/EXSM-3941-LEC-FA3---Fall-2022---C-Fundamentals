771ae2281ffcdadff9df63c911fe72fbb1e4f105
James Grieve <jgrieve@ualberta.ca>
2022-11-22 19:47:21 -0700

    Dates and times;

--- src/EXSM3941/Program.cs
+++ src/EXSM3941/Program.cs
@@ -4,47 +4,16 @@
     {
         static void Main(string[] args)
         {
-            // Initialize arrays with a length, which will fill them with the default value.
-            List<string> myList = new List<string>();
-            List<int> myIntList = new List<int>();
-
-            List<bool> myBoolList = new List<bool>() { true, false, false, true, false };
+            DateOnly christmas = new DateOnly(2000, 12, 25);
+            DateTime now = DateTime.Now;
 
 
-            List<string> userEntries = new List<string>();
-            // Start off with an "empty" array.
-            string userChoice = "";
-            do
+            Console.WriteLine(now);
+            Console.WriteLine($"{christmas}, {christmas.DayOfYear} days past new years. It was a {christmas.DayOfWeek}. {christmas.DayNumber} days AD.");
+            Console.WriteLine(DateTime.Parse(christmas.ToString()));
 
-            {
-                Console.WriteLine("1. Add\n2. Clear\n3. View\n4. Exit");
-                Console.Write("Choose: ");
-                userChoice = Console.ReadLine().Trim();
-                switch (userChoice)
-                {
-                    case "1":
-                        Console.Write("Enter what you'd like to add: ");
-                        // Set the "next" entry to what they enter.
-                        userEntries.Add(Console.ReadLine().Trim());
-                        break;
-                    case "2":
-                        userEntries.Clear();
-                        break;
-                    case "3":
-                        foreach (string item in userEntries)
-                        {
-                            Console.WriteLine(item);
-                        }
-                        break;
-                    case "4":
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input please try again.");
-                        break;
-                }
-
-            } while (userChoice != "4");
-
+            TimeSpan mySpan = new TimeSpan(10, 5, 30, 15);
+            Console.WriteLine(now.Add(mySpan));
         }
     }
 }

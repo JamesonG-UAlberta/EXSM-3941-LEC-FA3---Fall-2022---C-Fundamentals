44d474d8102dc11fd50b53531b7a708c8b546654
James Grieve <jgrieve@ualberta.ca>
2022-11-22 19:32:19 -0700

    List demo;

--- src/EXSM3941/Program.cs
+++ src/EXSM3941/Program.cs
@@ -5,48 +5,35 @@
         static void Main(string[] args)
         {
             // Initialize arrays with a length, which will fill them with the default value.
-            string[] myArray = new string[10];
-            int[] myIntArray = new int[5];
+            List<string> myList = new List<string>();
+            List<int> myIntList = new List<int>();
 
-            bool[] myBoolArray = new bool[] { true, false, false, true, false };
+            List<bool> myBoolList = new List<bool>() { true, false, false, true, false };
 
 
-            string[] userEntries = new string[5];
+            List<string> userEntries = new List<string>();
             // Start off with an "empty" array.
-            int userEntriesLogicalSize = 0;
             string userChoice = "";
             do
 
             {
-                Console.WriteLine("1. Add\n2. Clear\n3. View\n4.Exit");
+                Console.WriteLine("1. Add\n2. Clear\n3. View\n4. Exit");
                 Console.Write("Choose: ");
                 userChoice = Console.ReadLine().Trim();
-                switch(userChoice)
+                switch (userChoice)
                 {
                     case "1":
-                        if (userEntriesLogicalSize >= userEntries.Length)
-                        {
-                            Console.WriteLine("The array is full.");
-                        }
-                        else
-                        { 
-                            Console.Write("Enter what you'd like to add: ");
-                            // Set the "next" entry to what they enter.
-                            userEntries[userEntriesLogicalSize] = Console.ReadLine().Trim();
-                            userEntriesLogicalSize++;
-                        }
-                        
+                        Console.Write("Enter what you'd like to add: ");
+                        // Set the "next" entry to what they enter.
+                        userEntries.Add(Console.ReadLine().Trim());
                         break;
                     case "2":
-                        // Reset logical size to zero, which limits our output loop to not show the "garbage" data.
-                        // It will be overwritten with new adds.
-                        userEntriesLogicalSize = 0;
+                        userEntries.Clear();
                         break;
                     case "3":
-                        // Show up to the the logical size, nothing more.
-                        for (int i = 0; i < userEntriesLogicalSize; i++)
+                        foreach (string item in userEntries)
                         {
-                            Console.WriteLine(userEntries[i]);
+                            Console.WriteLine(item);
                         }
                         break;
                     case "4":
@@ -54,7 +41,7 @@
                     default:
                         Console.WriteLine("Invalid input please try again.");
                         break;
-                } 
+                }
 
             } while (userChoice != "4");
 

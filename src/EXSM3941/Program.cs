892ee6ff81871a4f6bb4370c60df511541986189
James Grieve <jgrieve@ualberta.ca>
2022-12-06 19:30:38 -0700

    Stack and Queue;

--- src/EXSM3941/Program.cs
+++ src/EXSM3941/Program.cs
@@ -10,12 +10,14 @@
             myList.Add("123");
             myList.Add("123");
 
+            // Cannot contain duplicate items. If you try, it will ignore the add.
             HashSet<string> myHashSet = new HashSet<string>();
             myHashSet.Add("Hello, world!");
             myHashSet.Add("Test.");
             myHashSet.Add("123");
             myHashSet.Add("123");
 
+            // Cannot contain duplicate items, like above, but also sorts by default.
             SortedSet<string> mySortedSet = new SortedSet<string>();
             mySortedSet.Add("Hello, world!");
             mySortedSet.Add("Test.");
@@ -27,6 +29,27 @@
             Console.WriteLine(myHashSet.ToArray()[1]);
             Console.WriteLine(mySortedSet.ElementAt(1));
             Console.WriteLine(mySortedSet.ToArray()[1]);
+
+
+
+            Stack<string> myStack = new Stack<string>();
+            Queue<string> myQueue = new Queue<string>();
+
+            // Adding
+            myStack.Push("A new box.");
+            myQueue.Enqueue("A person in line.");
+
+            // Looking
+            myStack.Peek(); // "A new box."
+            myQueue.Peek(); // "A person in line."
+
+            // Processing
+            myStack.Pop(); // "A new box."
+            myQueue.Dequeue(); // "A person in line."
+
+            
+
+
         }
     }
 }
